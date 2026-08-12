da8017cc3cf546fdb2aa0cd28d9b50b8119ba192
Kojo-Abaidoo <benjaminabaidoo5@gmail.com>
2026-08-12 13:27:22 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var nav = document.getElementById("navLinks");
+    if (nav.style.display === "flex") {
+       nav.style.display = "none";
+    } 
+    else {
+        nav.style.display = "flex";
+    }
+}
